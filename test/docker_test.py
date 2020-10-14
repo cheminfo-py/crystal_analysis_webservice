74d6e362d20c9408194502a628892eeeedd2b0ba
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-14 19:31:28 +0200

    chore: fix docker test

--- test/docker_test.py
+++ test/docker_test.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """Test if the API responds in the Docker image"""
 import sys
-import os
-import requests
 
-JULIAPACKAGE = os.getenv("JULIAPACKAGE")
+import requests
+import os
+import json
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 r = requests.get("http://localhost:8091/version/")
@@ -15,8 +17,15 @@ else:
     print("error")
     sys.exit(1)
 
-if JULIAPACKAGE:
-    print(JULIAPACKAGE)
+with open(os.path.join(THIS_DIR, "HKUST-1.cif"), "r") as fh:
+    content = fh.read()
+r = requests.post(
+    "http://localhost:8091/topology/", data=json.dumps({"fileContent": content})
+)
+response = r.json()
+
+if response["rcsr_name"] == "tbo":
+    print("tbo found for HKUST")
     sys.exit(0)
 else:
     print("error")
