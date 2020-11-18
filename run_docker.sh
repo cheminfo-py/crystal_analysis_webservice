f519a54a6ac3fc03b8725813f47441e9a2615878
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-11-19 00:57:50 +0100

    chore: update docker runscript

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "connect to http://localhost:8091"
-docker run -d -p 8091:8091 -e PORT=8091 --rm --name=crystal_analysis_webserviceinstance crystal_analysis_webservice
+docker run -d -p 8091:8091 -e PORT=8091 -e WORKERS=2 -e CONCURRENCY_LIMIT=2 --rm --name=crystal_analysis_webserviceinstance crystal_analysis_webservice
