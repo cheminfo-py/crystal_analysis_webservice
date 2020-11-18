55b238d631bd39e0fc9f7e3550956ab28bb13af7
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-11-18 13:17:55 +0100

    chore: remove versioneer

--- crystal_analysis_webservice/__init__.py
+++ crystal_analysis_webservice/__init__.py
@@ -3,12 +3,8 @@ crystal_analysis_webservice
 Expose the topology analysis tool developed by C Coudert's group
 """
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+__version__ = "0.2.0"
+
 
 # Add imports here
 from .crystal_analysis_webservice import *
